ed53e3b74db9ed34a86532fe7d2dc662a9edf2bb
snayrouz <samuel.nayrouz@gmail.com>
2017-07-26 09:08:19 -0600

    Add hash to participants

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -8,8 +8,13 @@ class ActivityTest < Minitest::Test
   def test_it_exists
     a = Activity.new("Brunch")
 
-    assert_instance_of Activity, a 
+    assert_instance_of Activity, a
   end
 
+  def test_starts_with_no_participants
+    a = Activity.new("Brunch")
+
+    assert_equal Hash.new , a.participants
+  end
 
 end

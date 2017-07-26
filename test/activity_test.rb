e0ca6a2998eea68ba7f0ae82194925ed7ac776f8
snayrouz <samuel.nayrouz@gmail.com>
2017-07-26 09:04:03 -0600

    Initial commit

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,12 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    a = Activity.new("Brunch")
+
+    assert_instance_of Activity, a 
+  end
+
+
 end
